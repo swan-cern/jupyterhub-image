94fd4e8e6f3a7741e78af21c41b6d25a34e96b94
Igor Tkachenko <tia@grid.kiae.ru>
2017-04-25 11:35:23 +0200

    Added user groups to ldap configuration

--- openldap.d/openldap_addusers.sh
+++ openldap.d/openldap_addusers.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+USER_Ns=`seq 0 9`
 ACTION_FILE='/tmp/action.ldif'
 ldapadd_macro  () {
   /usr/bin/ldapadd -x -H ldap://ldap -D "cn=admin,dc=example,dc=org" -w admin -f $ACTION_FILE
@@ -10,7 +11,7 @@ sleep 15
 
 # Create entries for dummy users on the ldap server
 echo "Configuring demo users on LDAP server..."
-for i in {0..9};
+for i in $USER_Ns;
 do
 	# Make a ldif file for each user
 	echo "dn: uid=user$i,dc=example,dc=org
@@ -53,6 +54,50 @@ shadowMax: 0
 shadowWarning: 0" > $ACTION_FILE
 ldapadd_macro
 ldappasswd -x -H ldap://ldap -D "cn=admin,dc=example,dc=org" -w admin "uid=dummy_admin,dc=example,dc=org" -s adminadmin
+
+# Create user groups
+echo "Creating groups branch.."
+
+cat >$ACTION_FILE <<EOF
+dn: ou=groups,dc=example,dc=org
+ou: groups
+description: generic groups branch
+objectclass: top
+objectclass:organizationalunit
+EOF
+ldapadd_macro
+
+echo Creating groups...
+for i in 1 2; do
+  cat <<EOG
+dn: cn=group$i,ou=groups,dc=example,dc=org
+objectClass: top
+objectClass: posixGroup
+gidNumber: 300$i
+
+EOG
+done > $ACTION_FILE
+ldapadd_macro
+
+for i in $USER_Ns; do
+  if [ $i -eq 0 -o $i -eq 9 ]; then
+    GROUPS="1 2"
+  elif [ $i -lt 5 ]; then
+    GROUPS="1"
+  else
+    GROUPS="2"
+  fi
+  for g in $GROUPS; do
+    cat <<EOM
+dn: cn=group$g,ou=groups,dc=example,dc=org
+changetype: modify
+add: memberuid
+memberuid: user$i
+
+EOM
+  done
+done >$ACTION_FILE
+ldapmodify -x -H ldap://ldap -D "cn=admin,dc=example,dc=org" -w admin -f $ACTION_FILE
 
 # Clean up
 rm -f $ACTION_FILE
