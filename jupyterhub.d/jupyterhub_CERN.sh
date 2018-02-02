6384e3b6c54bffeecdf5645732b9ab23fac917ac
Enrico Bocchi <enrico.bocchi@cern.ch>
2018-02-02 18:06:47 +0100

    Bugfix in directory path when creating the archive

--- jupyterhub.d/jupyterhub_CERN.sh
+++ jupyterhub.d/jupyterhub_CERN.sh
@@ -10,6 +10,9 @@
 # Docker image. Please, consider to add the latest version of the archives to
 # the Boxed repo so to keep it consistent with the content of the Docker image.
 
+
+set -o errexit	# Bail out on all errors immediately
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TMP_FOLDER="./tmp"
 OUT_FOLDER="./jupyterhub_CERN"
@@ -21,12 +24,12 @@ git clone https://:@gitlab.cern.ch:8443/dmaas/jupyterhub.git $TMP_FOLDER"/jupyte
 
 # Prepare the files to build the Docker images
 mkdir -p $OUT_FOLDER
-tar -cz -f $OUT_FOLDER"/CERNHandlers.tar.gz" $TMP_FOLDER"/jupyterhub/CERNHandlers"
-tar -cz -f $OUT_FOLDER"/CERNSpawner.tar.gz" $TMP_FOLDER"/jupyterhub/CERNSpawner" 
-tar -cz -f $OUT_FOLDER"/CERNTemplates.tar.gz" $TMP_FOLDER"/jupyterhub/templates"           
-tar -cz -f $OUT_FOLDER"/CERNLogos.tar.gz" $TMP_FOLDER"/jupyterhub/logo"                    
-cp $TMP_FOLDER"/manifest/code/templates/jupyterhub/jupyterhub_form.html.erb" $OUT_FOLDER"/jupyterhub_form.html.erb"
-cp $TMP_FOLDER"/jupyterhub/scripts/start_jupyterhub.py" $OUT_FOLDER"/start_jupyterhub.py"
+tar -cz -f $OUT_FOLDER"/CERNHandlers.tar.gz" -C $TMP_FOLDER"/jupyterhub" CERNHandlers
+tar -cz -f $OUT_FOLDER"/CERNSpawner.tar.gz" -C $TMP_FOLDER"/jupyterhub" CERNSpawner
+tar -cz -f $OUT_FOLDER"/CERNTemplates.tar.gz" -C $TMP_FOLDER"/jupyterhub" templates
+tar -cz -f $OUT_FOLDER"/CERNLogos.tar.gz" -C $TMP_FOLDER"/jupyterhub" logo
+/bin/cp $TMP_FOLDER"/manifest/code/templates/jupyterhub/jupyterhub_form.html.erb" $OUT_FOLDER"/jupyterhub_form.html.erb"
+/bin/cp $TMP_FOLDER"/jupyterhub/scripts/start_jupyterhub.py" $OUT_FOLDER"/start_jupyterhub.py"
 
 # Clean-up temporary files
 rm -rf $TMP_FOLDER
